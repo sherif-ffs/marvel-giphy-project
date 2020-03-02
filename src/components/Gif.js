ec59e2175bb6c4b90037c9c72c9ad6cf89a126b9
Sherif Elmetwally <sge5@njit.edu>
2020-03-02 16:54:13 -0500

    FEATURE: add framer-motion and animate gifs and gif-container

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,35 +1,39 @@
 import React from 'react';
 
+import { motion } from "framer-motion";
+
 import styled from 'styled-components';
 import '../styles/Gif/gif.css'
 
-// const GifWrapper = styled.div`
-//     background: ${props => props.url.complete ? props.url : 'red'};
-//     height: 70%!important;
-//     width: 100%;
-//     border-radius: 5px;
-//     box-shadow: 0px 1px 10px rgba(34, 34, 34,.3);
-// `
-
 class Gif extends React.Component {
+
     render() {
-        console.log('this.props: ', this.props)
+        console.log('this.props; ', this.props)
         return (
-            <div className="gif-wrapper">
-                <div 
-                className="gif-img-wrapper"
-                style={
-                    {
-                        background: `${this.props.url.complete ? this.props.url : '#f5f5f5'}`
-                    }
-                }
+            <div 
+                className="gif-wrapper"
                 >
-                    <img src={this.props.url} alt="gif" className="gif-wrapper__image"></img>
-                </div>
+                <motion.div 
+                    className="gif-img-wrapper"
+                    whileHover={{ scale: 1, rotate: 2 }}
+                    whileTap={{
+                        scale: 0.9,
+                        rotate: -3,
+                    }}
+                    style={
+                        {
+                            background: `${this.props.activeUrl ? this.props.activeUrl : '#f5f5f5'}`
+                        }
+                    }
+                >
+                    <img src={this.props.activeUrl} alt="gif" className="gif-wrapper__image"></img>
+                </motion.div>
                 <h1 className="gif-wrapper__title">{this.props.title}</h1>
             </div>
+
         )
     }
 }
 
 export default Gif
+
--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,37 +1,70 @@
 import React from 'react';
 
-import Gif from './Gif'
+import Gif from './Gif';
+import { motion } from "framer-motion";
 import Button from '@material-ui/core/Button';
 
 import '../styles/MainView/mainView.css'
+
+
+const container = {
+    hidden: { opacity: 1, scale: .8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delay: 0.3,
+        when: "beforeChildren",
+        staggerChildren: 0.1
+      }
+    }
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+
 class MainView extends React.Component {
 
+    scrollToTop = () => {
+        window.scrollTo(0,0);
+    }
+
     render() {
-        console.log('this.props: ', this.props)
         let gifs = this.props.trendingGifs
 
         let trendingGifs;
-        
+
         if (typeof gifs !== 'undefined') {
             console.log('gifs: ', gifs)
             trendingGifs = gifs.map(gif => {
-                return(
-                    <Gif 
-                        className="gif-container"
-                        title={gif.title}
-                        url={gif.images.downsized_medium.url}
-                    >
-                    </Gif>
-                )
+                    return(
+                        <Gif 
+                            className="item"
+                            variants={item}
+                            title={gif.title}
+                            activeUrl={gif.images.original.webp}
+                        >
+                        </Gif>
+                    )
             })
         }
-
+        
         return(
             <React.Fragment>
                 <section className="main-view">
-                    <div className="gif-container">
+                    <motion.div 
+                        className="container"
+                        variants={container}
+                        initial="hidden"
+                        animate="visible"
+                    >
                         {trendingGifs}
-                    </div>
+                    </motion.div>
                     <Button 
                         onClick={this.props.onClick} 
                         variant="contained" 
@@ -39,7 +72,15 @@ class MainView extends React.Component {
                         className="gif-container__button"
                     >
                     Load More Gifs
-                    </Button>                
+                    </Button>
+                    <Button 
+                        onClick={this.scrollToTop} 
+                        variant="contained" 
+                        color="secondary" 
+                        className="gif-container__button"
+                    >
+                    Back to top
+                    </Button>          
                 </section>
             </React.Fragment>
         ) 
